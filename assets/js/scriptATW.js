763e90b5fbcacbf31a24748e8008b4f0be589a75
Riley Jacobson <98298991+Rileyj53@users.noreply.github.com>
2023-01-13 16:05:38 -0500

    Moved files to this repo and changed the structure

---
+++ assets/js/scriptATW.js
@@ -0,0 +1,105 @@
+//Popup code adapted from (“A Very Simple Popup Box - HTML, CSS, JavaScript”)
+
+//Mexico
+$(window).load(function () {
+  $(".trigger_popup_Mexico").click(function () {
+    $(".hover_bkgr_Mexico").show();
+  });
+  $(".hover_bkgr_Mexico").click(function () {
+    $(".hover_bkgr_Mexico").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Mexico").hide();
+  });
+});
+
+//Puerto Rico
+$(window).load(function () {
+  $(".trigger_popup_Puerto").click(function () {
+    $(".hover_bkgr_Puerto").show();
+  });
+  $(".hover_bkgr_Puerto").click(function () {
+    $(".hover_bkgr_Puerto").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Puerto").hide();
+  });
+});
+
+//Guatemala
+$(window).load(function () {
+  $(".trigger_popup_Guatemala").click(function () {
+    $(".hover_bkgr_Guatemala").show();
+  });
+  $(".hover_bkgr_Guatemala").click(function () {
+    $(".hover_bkgr_Guatemala").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Guatemala").hide();
+  });
+});
+
+//Costa Rica
+$(window).load(function () {
+  $(".trigger_popup_Costa").click(function () {
+    $(".hover_bkgr_Costa").show();
+  });
+  $(".hover_bkgr_Costa").click(function () {
+    $(".hover_bkgr_Costa").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Costa").hide();
+  });
+});
+
+//Colombia
+$(window).load(function () {
+  $(".trigger_popup_Colombia").click(function () {
+    $(".hover_bkgr_Colombia").show();
+  });
+  $(".hover_bkgr_Colombia").click(function () {
+    $(".hover_bkgr_Colombia").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Colombia").hide();
+  });
+});
+
+//Ethiopia
+$(window).load(function () {
+  $(".trigger_popup_Ethiopia").click(function () {
+    $(".hover_bkgr_Ethiopia").show();
+  });
+  $(".hover_bkgr_Ethiopia").click(function () {
+    $(".hover_bkgr_Ethiopia").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Ethiopia").hide();
+  });
+});
+
+//Brazil
+$(window).load(function () {
+  $(".trigger_popup_Brazil").click(function () {
+    $(".hover_bkgr_Brazil").show();
+  });
+  $(".hover_bkgr_Brazil").click(function () {
+    $(".hover_bkgr_Brazil").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Brazil").hide();
+  });
+});
+
+//indonesia
+$(window).load(function () {
+  $(".trigger_popup_indonesia").click(function () {
+    $(".hover_bkgr_indonesia").show();
+  });
+  $(".hover_bkgr_indonesia").click(function () {
+    $(".hover_bkgr_indonesia").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_indonesia").hide();
+  });
+});
---
+++ assets/js/scriptRECP.js
@@ -0,0 +1,103 @@
+//Carajillo
+$(window).load(function () {
+  $(".trigger_popup_Carajillo").click(function () {
+    $(".hover_bkgr_Carajillo").show();
+  });
+  $(".hover_bkgr_Carajillo").click(function () {
+    $(".hover_bkgr_Carajillo").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Carajillo").hide();
+  });
+});
+
+//Cortadito
+$(window).load(function () {
+  $(".trigger_popup_Cortadito").click(function () {
+    $(".hover_bkgr_Cortadito").show();
+  });
+  $(".hover_bkgr_Cortadito").click(function () {
+    $(".hover_bkgr_Cortadito").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Cortadito").hide();
+  });
+});
+
+//cafe-con-leche
+$(window).load(function () {
+  $(".trigger_popup_cafe-con-leche").click(function () {
+    $(".hover_bkgr_cafe-con-leche").show();
+  });
+  $(".hover_bkgr_cafe-con-leche").click(function () {
+    $(".hover_bkgr_cafe-con-leche").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_cafe-con-leche").hide();
+  });
+});
+
+//Cafe-De-Olla
+$(window).load(function () {
+  $(".trigger_popup_Cafe-De-Olla").click(function () {
+    $(".hover_bkgr_Cafe-De-Olla").show();
+  });
+  $(".hover_bkgr_Cafe-De-Olla").click(function () {
+    $(".hover_bkgr_Cafe-De-Olla").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Cafe-De-Olla").hide();
+  });
+});
+
+//Kopi-Joss
+$(window).load(function () {
+  $(".trigger_popup_Kopi-Joss").click(function () {
+    $(".hover_bkgr_Kopi-Joss").show();
+  });
+  $(".hover_bkgr_Kopi-Joss").click(function () {
+    $(".hover_bkgr_Kopi-Joss").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Kopi-Joss").hide();
+  });
+});
+
+//Es-Alpukat-Kopi
+$(window).load(function () {
+  $(".trigger_popup_Es-Alpukat-Kopi").click(function () {
+    $(".hover_bkgr_Es-Alpukat-Kopi").show();
+  });
+  $(".hover_bkgr_Es-Alpukat-Kopi").click(function () {
+    $(".hover_bkgr_Es-Alpukat-Kopi").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Es-Alpukat-Kopi").hide();
+  });
+});
+
+//Mocha-Cola
+$(window).load(function () {
+  $(".trigger_popup_Mocha-Cola").click(function () {
+    $(".hover_bkgr_Mocha-Cola").show();
+  });
+  $(".hover_bkgr_Mocha-Cola").click(function () {
+    $(".hover_bkgr_Mocha-Cola").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Mocha-Cola").hide();
+  });
+});
+
+//Cafezinho
+$(window).load(function () {
+  $(".trigger_popup_Cafezinho").click(function () {
+    $(".hover_bkgr_Cafezinho").show();
+  });
+  $(".hover_bkgr_Cafezinho").click(function () {
+    $(".hover_bkgr_Cafezinho").hide();
+  });
+  $(".popupCloseButton").click(function () {
+    $(".hover_bkgr_Cafezinho").hide();
+  });
+});
